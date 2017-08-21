f7219aeccd26032f4bde06faeef8a68692efe478
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2017-08-22 01:09:03 +0300

    Minor edits

--- make.bat
+++ make.bat
@@ -103,7 +103,7 @@ if "%1" == "gh-pages" (
         RD /S /Q _static
         RD /S /Q _images
         DEL /Q *.*
-        git checkout master data source make.bat Makefile
+        git checkout master source make.bat Makefile
         git reset HEAD
         dir
         PAUSE
