45e42a2f5c61cc619b85e0a186909bb52d4a489b
Dave Whipp <david.whipp@helsinki.fi>
2018-08-23 16:44:34 +0300

    Added theme overrides and more sphinx_rtd_theme stuff

--- source/conf.py
+++ source/conf.py
@@ -82,8 +82,14 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+import sphinx_rtd_theme
+
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+def setup(app):
+    app.add_stylesheet('theme_overrides.css')
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
