42de8e431962ba092db78eb59fb8668116cb273a
Dave Whipp <david.whipp@helsinki.fi>
2020-08-28 20:42:33 +0300

    Add sphinx thebe extension

--- source/conf.py
+++ source/conf.py
@@ -38,6 +38,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
     'sphinx.ext.todo',
+    "sphinx_thebe",
     'sphinxcontrib.googleanalytics',
     'IPython.sphinxext.ipython_console_highlighting',
     'IPython.sphinxext.ipython_directive',
