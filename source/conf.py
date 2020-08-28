2d7ad6069b470208df5d7b9144669e21ba784c94
Dave Whipp <david.whipp@helsinki.fi>
2020-08-28 16:00:52 +0300

    Fix binder url

--- source/conf.py
+++ source/conf.py
@@ -109,7 +109,7 @@ html_theme_options = {
     #"google_analytics_id": "UA-159257488-1",
     "use_edit_page_button": True,
     "launch_buttons": {
-        "binderhub_url": "https://mybinder.org/",
+        "binderhub_url": "https://mybinder.org",
         "thebelab": True,
         "notebook_interface": "jupyterlab",
     "collapse_navigation" : False
