221eea56865a303773692e2f6eb6efcd904e8128
Dave Whipp <david.whipp@helsinki.fi>
2020-08-28 09:32:36 +0300

    Add Geo-Python logo

--- source/conf.py
+++ source/conf.py
@@ -99,6 +99,8 @@ pygments_style = 'sphinx'
 # =======================
 
 html_theme = 'sphinx_book_theme'
+html_logo = "_static/geopython.png"
+html_title = ""
 
 html_theme_options = {
     #"external_links": [],
