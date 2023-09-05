e8f6018b81a671614128af577951240543cc736e
Dave Whipp <david.whipp@helsinki.fi>
2023-09-05 20:39:40 +0200

    Add custom css for icons

--- source/conf.py
+++ source/conf.py
@@ -130,6 +130,10 @@ html_last_updated_fmt = ""
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_css_files = [
+    'css/custom.css',
+]
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
